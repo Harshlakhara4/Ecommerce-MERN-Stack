729f1d187dc2a2322746be54a7ece59d270121b0
Harshlakhara4 <lakharaharsh4@gmail.com>
2025-02-07 00:09:47 +0530

    Update auth-controller.js

--- server/controller/auth/auth-controller.js
+++ server/controller/auth/auth-controller.js
@@ -108,6 +108,7 @@ const logoutUser = (req, res) => {
 
 const authMiddleware = async (req, res, next) => {
     const token = req.cookies.token;
+    console.log(req.body);
     if (!token)
         return res.status(401).json({
            success: false,
