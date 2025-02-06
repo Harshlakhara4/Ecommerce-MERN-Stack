aa6d668630a67264325402c0c1117650ccb0f7d2
Harshlakhara4 <lakharaharsh4@gmail.com>
2025-02-06 23:20:50 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "*",
     method: ["GET", "POST", "DELETE", "PUT"],
     allowedHEaders: [
       "Content-Type",
