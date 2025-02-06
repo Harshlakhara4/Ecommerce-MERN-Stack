53df5f68326b15de8eb8263f3070b380311ca051
Harshlakhara4 <lakharaharsh4@gmail.com>
2025-02-06 23:01:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-mern-stack-ten.vercel.app",
     method: ["GET", "POST", "DELETE", "PUT"],
     allowedHEaders: [
       "Content-Type",
