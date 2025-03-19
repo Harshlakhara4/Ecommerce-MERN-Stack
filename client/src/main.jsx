06f41646738d32064a2e01519c52553fe35fc67e
Harshlakhara4 <lakharaharsh4@gmail.com>
2025-03-19 11:11:29 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,18 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import App from "./App.jsx";
 import store from "./store/store.js";
 import { Toaster } from "./components/ui/toaster.jsx";
+import "./index.css";
 
+const root = document.getElementById("root");
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={store}>
+createRoot(root).render(
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-       <Toaster />
-    </Provider>
-  </BrowserRouter>
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 );
